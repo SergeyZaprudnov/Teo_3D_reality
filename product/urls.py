bf4b4d92765b3a10264a4b4729ad06c4caf98b7e
sergey <szaprudnov@hotmail.com>
2024-01-31 12:54:15 +0300

    correction made

--- product/urls.py
+++ product/urls.py
@@ -2,10 +2,11 @@ from django import views
 from django.urls import path
 
 from product.apps import ProductConfig
-from product.views import ProductListView
+from product.views import ProductListView, IndexView
 
 app_name = ProductConfig.name
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('product/', ProductListView.as_view(), name='product-list'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,9 +1,14 @@
-
+from django.shortcuts import render
 from django.views.generic import ListView
 
 from product.models import Product
 
 
+class IndexView(ListView):
+    model = Product
+    template_name = 'index.html'
+
+
 class ProductListView(ListView):
     model = Product
-    template_name = 'product_list.html'
+    template_name = 'product/product_list.html'
