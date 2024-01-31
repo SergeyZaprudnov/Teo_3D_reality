45fb481051e8b9ddde8a6cbcd3607745d739c206
sergey <szaprudnov@hotmail.com>
2024-01-31 11:55:04 +0300

    setting up the administrative panel

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'price', 'image', 'pub_date', 'views')
+    search_fields = ('title', 'price')
+    list_filter = ('title', 'pub_date', 'price')
