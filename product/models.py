15621d9355da768306729d4645719f7011f6d27d
sergey <szaprudnov@hotmail.com>
2024-01-31 11:45:16 +0300

    add app

--- product/models.py
+++ product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/', verbose_name='Изображение', **NULLABLE)
+    pub_date = models.DateField(default=timezone.now, verbose_name='Дата публикации')
+    views = models.IntegerField(default=0, verbose_name='Просмотры')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
