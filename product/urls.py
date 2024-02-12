6e72d88ba088d72bf88ad01a033bc09559064833
sergey <szaprudnov@hotmail.com>
2024-02-12 16:04:29 +0300

    introduced path representation

--- product/urls.py
+++ product/urls.py
@@ -2,11 +2,12 @@ from django import views
 from django.urls import path
 
 from product.apps import ProductConfig
-from product.views import ProductListView, IndexView
+from product.views import ProductListView, IndexView, ProductDetailView
 
 app_name = ProductConfig.name
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('product/', ProductListView.as_view(), name='product'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
