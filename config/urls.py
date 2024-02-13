a30de8e5bbc9b8890de3eb57c261319ddaadbd3b
sergey <szaprudnov@hotmail.com>
2024-02-13 12:57:05 +0300

    added path view

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('product.urls', namespace='product'))
+    path('', include('product.urls', namespace='product')),
+    path('user/', include('user.urls', namespace='users')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
