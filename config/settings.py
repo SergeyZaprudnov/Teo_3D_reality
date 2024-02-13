cd848176446ae032da71c001c1a90f08fd74ad2b
sergey <szaprudnov@hotmail.com>
2024-02-13 12:48:48 +0300

    added user application to settings configuration

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'product',  # добавлено приложение
+    'product',  # добавлено приложение продукта
+    'user',   # добавлено приложения пользователя
 ]
 
 MIDDLEWARE = [
