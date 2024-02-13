ef8f8706119f7251981e9eebc7c0f4939ad41f8e
sergey <szaprudnov@hotmail.com>
2024-02-13 22:25:02 +0300

    added path view

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UserConfig
+from users.views import UserDetailView
 
 app_name = UserConfig.name
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', LoginView.as_view(template_name='user/user_login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
 ]
