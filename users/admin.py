3514203262466b08f15b040c4defad8ccc24412f
sergey <szaprudnov@hotmail.com>
2024-02-13 14:13:32 +0300

    the user's administrative panel has been configured

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'surname2', 'address', 'email', 'phone')
+    search_fields = ('name', 'surname', 'surname2', 'address', 'email', 'phone')
+    list_filter = ('email', 'phone')
